c5bf7cccd84e2a4cf6535603ad78c28939243647
callumahern <callum.ahern@simplybusiness.co.uk>
2022-12-19 10:35:59 +0000

    refactored for instance variables

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,15 +1,15 @@
 class Sale
   attr_accessor :display
 
-  def initialize(display)
-    self.display = display
+  def initialize(display = Display.new)
+    @display = display
   end
 
   def on_barcode(string)
     if string == '12345'
-      display.set_price('7.95')
+      @display.set_price('7.95')
     else 
-      display.set_price('12.50')
+      @display.set_price('12.50')
     end
   end
 end
