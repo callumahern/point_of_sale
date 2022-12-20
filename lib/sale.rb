ad674ddc15bfcf020e23fa4fa2e422621804cdeb
callumahern <callum.ahern@simplybusiness.co.uk>
2022-12-20 07:43:05 +0000

    naming for barcode hash

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Sale
-  def initialize(display = Display.new, prices_by_barcode = { '12345' => '£7.95', '23456' => '£12.50' })
+  def initialize(display = Display.new, catalogue = { '12345' => '£7.95', '23456' => '£12.50' })
     @display = display
-    @prices_by_barcode = prices_by_barcode
+    @prices_by_barcode = catalogue
   end
 
   def on_barcode(barcode)
