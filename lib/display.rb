c8f1b5b765775e528dedb657bf1aae9b11c95995
callumahern <callum.ahern@simplybusiness.co.uk>
2022-12-21 10:29:40 +0000

    stopped at failing test for review, moving private methods to public class

--- lib/display.rb
+++ lib/display.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
 class Display
+
   def get_price
     @price
   end
 
   def set_price(string)
     @price = string
+  end
+
+  def display_price(barcode)
+    @display.set_price(@prices_by_barcode[barcode])
+  end
+
+  def display_product_not_found_message(barcode)
+    @display.set_price("Product not found for #{barcode}")
+  end
+
+  def display_empty_barcode_message
+    @display.set_price('Scanning error: empty barcode')
   end
 end
--- lib/sale.rb
+++ lib/sale.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Sale
-  def initialize(display = Display.new, catalogue = { '12345' => '£7.95', '23456' => '£12.50' })
+  def initialize(display, catalogue = { '12345' => '£7.95', '23456' => '£12.50' })
     @display = display
     @prices_by_barcode = catalogue
   end
@@ -17,18 +17,6 @@ class Sale
   end
 
   private
-
-  def display_empty_barcode_message
-    @display.set_price('Scanning error: empty barcode')
-  end
-
-  def display_product_not_found_message(barcode)
-    @display.set_price("Product not found for #{barcode}")
-  end
-
-  def display_price(barcode)
-    @display.set_price(@prices_by_barcode[barcode])
-  end
 
   def has_barcode(barcode)
     @prices_by_barcode[barcode]
